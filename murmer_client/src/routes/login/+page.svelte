fce718ff2b30e8bbdce761387a85fcfebece2b2e
Tjark Frölje <40803319+Tenosiey@users.noreply.github.com>
2025-07-11 20:40:11 +0200

    Remove all styling

--- murmer_client/src/routes/login/+page.svelte
+++ murmer_client/src/routes/login/+page.svelte
@@ -18,8 +18,8 @@
   }
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen space-y-4">
-  <h1 class="text-2xl font-bold">Login</h1>
-  <input class="border p-2 rounded" bind:value={username} placeholder="Username" />
-  <button class="bg-blue-500 text-white px-4 py-2 rounded" on:click={login}>Login</button>
+<div>
+  <h1>Login</h1>
+  <input bind:value={username} placeholder="Username" />
+  <button on:click={login}>Login</button>
 </div>
